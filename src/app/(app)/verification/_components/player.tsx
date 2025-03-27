26abbb41da4fa274c5682d883412973c94a6f0f0
ericksenfloris148 <ericksenfloris148@gmail.com>
2025-03-27 12:10:03 -0300

    Update player.tsx

--- src/app/(app)/verification/_components/player.tsx
+++ src/app/(app)/verification/_components/player.tsx
@@ -1,92 +1,54 @@
 'use client'
 
-import { useState, useRef } from 'react'
+import { useEffect } from 'react'
 
 const Player = () => {
-  const videoRef = useRef<HTMLVideoElement | null>(null)
-  const [isMuted, setIsMuted] = useState(true)
-  const [firstUnmuteClick, setFirstUnmuteClick] = useState(true)
-  const [videoEnded, setVideoEnded] = useState(false) // Estado para controlar o fim do vídeo
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src =
+      'https://scripts.converteai.net/20448767-cfea-41f4-baa4-9dc0974d8a85/players/67e561a62d07477843fbcb30/player.js'
+    script.async = true
+    document.head.appendChild(script)
 
-  const toggleMute = () => {
-    if (videoRef.current) {
-      if (isMuted) {
-        videoRef.current.muted = false
-        if (firstUnmuteClick) {
-          videoRef.current.currentTime = 0
-          setFirstUnmuteClick(false)
-        }
-      } else {
-        videoRef.current.muted = true
-      }
-      setIsMuted(!isMuted)
+    return () => {
+      document.head.removeChild(script)
     }
-  }
-
-  const handleVideoEnd = () => {
-    setVideoEnded(true) // Quando o vídeo termina, atualiza o estado
-  }
+  }, [])
 
   return (
     <div
-      className="video-container"
+      id="vid_67e561a62d07477843fbcb30"
       style={{
         position: 'relative',
         width: '100%',
-        maxWidth: '100vw', // Garante que o vídeo não ultrapasse a largura da tela
-        aspectRatio: '16 / 9', // Mantém o formato 16:9
-        backgroundColor: '#000',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        overflow: 'hidden',
-        borderRadius: '15px',
+        padding: '56.25% 0 0',
       }}
     >
-      <video
-        ref={videoRef}
-        className="video"
-        autoPlay
-        muted
-        playsInline
-        onEnded={handleVideoEnd}
+      <img
+        id="thumb_67e561a62d07477843fbcb30"
+        src="https://images.converteai.net/20448767-cfea-41f4-baa4-9dc0974d8a85/players/67e561a62d07477843fbcb30/thumbnail.jpg"
         style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
           width: '100%',
           height: '100%',
-          objectFit: 'contain', // Mantém o vídeo inteiro sem cortes
-          borderRadius: '15px',
+          objectFit: 'cover',
+          display: 'block',
         }}
-      >
-        <source src="https://finalizepagamento.lat/video/media/pg.mp4" type="video/mp4" />
-        Seu navegador não suporta a tag de vídeo.
-      </video>
-
-      {/* Botão de mute/unmute (só aparece se o vídeo não terminou) */}
-      {!videoEnded && (
-        <button
-          onClick={toggleMute}
-          style={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            background: 'rgba(0, 0, 0, 0.5)',
-            color: 'white',
-            border: 'none',
-            padding: '15px',
-            fontSize: '24px',
-            borderRadius: '50%',
-            cursor: 'pointer',
-            opacity: 0.7,
-            transition: 'opacity 0.3s ease',
-          }}
-        >
-          {isMuted ? '🔊' : '🔇'}
-          <p style={{ fontSize: '10px', margin: 0 }}>
-            {isMuted ? 'Clique para Ouvir' : 'Clique para Mutar'}
-          </p>
-        </button>
-      )}
+        alt="thumbnail"
+      />
+      <div
+        id="backdrop_67e561a62d07477843fbcb30"
+        style={{
+          WebkitBackdropFilter: 'blur(5px)',
+          backdropFilter: 'blur(5px)',
+          position: 'absolute',
+          top: 0,
+          height: '100%',
+          width: '100%',
+        }}
+      />
     </div>
   )
 }
